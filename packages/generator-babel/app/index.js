b457c3040099cb1261acb77109d095858dd67349
Landon Schropp <schroppl@gmail.com>
2022-01-24 16:18:42 -0600

    Remove Babel plugins

--- packages/generator-babel/app/index.js
+++ packages/generator-babel/app/index.js
@@ -1,9 +1,4 @@
-const _ = require("lodash");
 const Generator = require("yeoman-generator");
-
-const PIPELINE_PLUGIN = [ "@babel/plugin-proposal-pipeline-operator", { "proposal": "smart" } ];
-const DECORATORS_PLUGIN = [ "@babel/plugin-proposal-decorators", { "legacy": true } ];
-const CLASS_PROPERITES_PLUGIN = [ "@babel/plugin-proposal-class-properties", { "loose": false } ];
 
 module.exports = class BabelGenerator extends Generator {
 
@@ -13,25 +8,6 @@ module.exports = class BabelGenerator extends Generator {
         type: "confirm",
         name: "react",
         message: "Is this a React project?"
-      },
-      {
-        type: "checkbox",
-        name: "plugins",
-        message: "Which plugins would you link to install?",
-        choices: [
-          {
-            name: "Pipeline Operator",
-            value: "pipeline"
-          },
-          {
-            name: "Decorators",
-            value: "decorators"
-          },
-          {
-            name: "Class Properties",
-            value: "classProperties"
-          }
-        ]
       }
     ]));
   }
@@ -48,28 +24,11 @@ module.exports = class BabelGenerator extends Generator {
     if (this.react) {
       await this.addDependencies([ "babel-preset-react-app" ]);
     }
-
-    if (_.includes(this.plugins, "pipeline")) {
-      await this.addDependencies([ "@babel/plugin-proposal-pipeline-operator" ]);
-    }
-
-    if (_.includes(this.plugins, "decorators")) {
-      await this.addDependencies([ "@babel/plugin-proposal-decorators" ]);
-    }
-
-    if (_.includes(this.plugins, "classProperties")) {
-      await this.addDependencies([ "@babel/plugin-proposal-class-properties" ]);
-    }
   }
 
   writing() {
     let babelConfiguration = {
-      "presets": [ this.react ? "babel-preset-react-app" : "@babel/preset-env" ],
-      "plugins": _.reject([
-        _.includes(this.plugins, "pipeline") ? PIPELINE_PLUGIN : null,
-        _.includes(this.plugins, "decorators") ? DECORATORS_PLUGIN : null,
-        _.includes(this.plugins, "classProperties") ? CLASS_PROPERITES_PLUGIN : null
-      ], _.isNil)
+      "presets": [ this.react ? "babel-preset-react-app" : "@babel/preset-env" ]
     };
 
     this.fs.writeJSON(
