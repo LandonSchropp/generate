b18391abf08f2f9f5e9fdfebe4d04a76c288f4fb
Landon Schropp <schroppl@gmail.com>
2025-08-31 22:47:36 -0700

    Make package.json path more explicit

--- src/initialize/index.js
+++ src/initialize/index.js
@@ -1,12 +1,12 @@
 import { getGitHubUsername, getGitUserName, getGitUserEmail } from "../utilities/git.js";
 import { packageManagerLockFile } from "../utilities/package-manager.js";
 import { readJson, pathExists } from "fs-extra/esm";
-import { basename } from "path";
+import { basename, join } from "path";
 
 export default async (plop) => {
-  // TODO: This slows down reading all of the generators. It would be nice if instead it could be
-  // done async inside of the prompts, but that's not currently allowed by Plop.
-  let packageJson = (await pathExists("package.json")) ? await readJson("package.json") : {};
+  let packageJsonPath = join(plop.getDestBasePath(), "package.json");
+  let packageJson = (await pathExists(packageJsonPath)) ? await readJson(packageJsonPath) : {};
+
   let gitHubUsername = await getGitHubUsername();
   let gitUserName = await getGitUserName();
   let gitUserEmail = await getGitUserEmail();
