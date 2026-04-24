788dc4ae3395f143608d85b77bb8fc2657f9cf28
Landon Schropp <schroppl@gmail.com>
2026-04-23 20:02:19 -0700

    Auto-reset mocks between tests

---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    restoreMocks: true,
+    mockReset: true,
+    clearMocks: true,
+    unstubGlobals: true,
+    unstubEnvs: true,
+  },
+});
