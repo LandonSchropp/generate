debabd162b6fbf62ab8672b251856cc658704c77
Landon Schropp <schroppl@gmail.com>
2026-04-24 14:13:36 -0700

    Use fs-extra for JSON reading in hasDependency

--- src/utilities/project.js
+++ src/utilities/project.js
@@ -1,16 +1,14 @@
-import { existsSync, readFileSync } from "node:fs";
+import { pathExistsSync, readJsonSync } from "fs-extra/esm";
 import { join } from "node:path";
 
 export function hasDependency(name, cwd = process.cwd()) {
   let packageJsonPath = join(cwd, "package.json");
 
-  if (!existsSync(packageJsonPath)) {
+  if (!pathExistsSync(packageJsonPath)) {
     return false;
   }
 
-  let { dependencies = {}, devDependencies = {} } = JSON.parse(
-    readFileSync(packageJsonPath, "utf8"),
-  );
+  let { dependencies = {}, devDependencies = {} } = readJsonSync(packageJsonPath);
 
   return name in dependencies || name in devDependencies;
 }
